0f5aa19c3e843f4a4d9f67c03a2e45be6431cad9
EloiGry <eloi.grychta@gmail.com>
2023-05-05 21:15:58 +0200

    modif

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -9,9 +9,7 @@ function App() {
       <Header/>
       <div className="h-[90vh] bg-lightBrown text-white text-center flex flex-col items-center">
         <h1 className="bg-gradient-to-r from-blue via-green-500 to-indigo-400 inline-block text-transparent bg-clip-text text-3xl font-semibold"> Mastermind </h1>
-        <button className="border-2 border-white py-1 px-3 rounded-sm w-32 hover:border-indigo-400 hover:text-indigo-400 duration-300">
-          <Link to='/play'> Play </Link>
-        </button>
+          <Link to='/play' className="border-2 border-white py-1 px-3 rounded-sm w-32 hover:border-indigo-400 hover:text-indigo-400 duration-300"> Play </Link>
       </div>
     </div>
   )
